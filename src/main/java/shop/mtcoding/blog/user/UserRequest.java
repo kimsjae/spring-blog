4dd78e830b55f159144a18abdf501f3841c9b226
kimseongjae <stwin755@gmail.com>
2024-02-05 12:56:20 +0900

    회원가입 구현하기 - JoinDTO 만들기

---
+++ src/main/java/shop/mtcoding/blog/user/UserRequest.java
@@ -0,0 +1,15 @@
+package shop.mtcoding.blog.user;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class UserRequest {
+    @Data
+    public static class JoinDTO {
+        private String username;
+        private String password;
+        private String email;
+    }
+
+
+}
